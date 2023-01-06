a5d443eda52de870b47849e94c84e5506614e53f
Danilo Vieira Costa <danilo.11235@outlook.com>
2023-01-06 07:26:36 -0300

    Correção no nome da variável.

--- Aula1/server.js
+++ Aula1/server.js
@@ -1,4 +1,4 @@
 const server_que_eu_criei = require("http");
-server_que_eu_criei.createServer((requeste, response) => {
+server_que_eu_criei.createServer((request, response) => {
     response.end('So testando...');
 }).listen(2000);
